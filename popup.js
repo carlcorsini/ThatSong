161aea5121af89336f5d569be91245e7cce8ed26
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-08-24 11:19:43 -0700

    basic functionality printhing to console. Having trouble writing data to seperate file

--- popup.js
+++ popup.js
@@ -1,14 +1,34 @@
 let changeColor = document.getElementById('changeColor')
-
+let scripty =
+  'console.log(document.getElementsByClassName("playbackTimeline__progressWrapper")[0].attributes[3].nodeValue, document.getElementsByClassName("playbackSoundBadge__titleLink sc-truncate")[0].attributes[2].nodeValue, document.getElementsByClassName("playbackSoundBadge__titleLink sc-truncate")[0].attributes[0].nodeValue);'
+// let script = 'chrome.storage.local.set({key: "value"});'
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color
   changeColor.setAttribute('value', data.color)
 })
 changeColor.onclick = function(element) {
   let color = element.target.value
+
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    // chrome.tabs.executeScript(tabs[0].id, {
+    //   code: scripty
+    // })
     chrome.tabs.executeScript(tabs[0].id, {
-      code: 'document.body.style.backgroundColor = "' + color + '";'
+      file: 'scripty.js'
     })
   })
 }
+
+// code: 'console.log(document.getElementsByClassName("playbackTimeline__progressWrapper")[0].attributes[3].nodeValue, document.getElementsByClassName("playbackSoundBadge__titleLink sc-truncate")[0].attributes[2].nodeValue, document.getElementsByClassName("playbackSoundBadge__titleLink sc-truncate")[0].attributes[0].nodeValue);'
+
+// let timestamp = document.getElementsByClassName(
+//   'playbackTimeline__progressWrapper'
+// )[0].attributes[3].nodeValue
+//
+// let title = document.getElementsByClassName(
+//   'playbackSoundBadge__titleLink sc-truncate'
+// )[0].attributes[2].nodeValue
+//
+// let url = document.getElementsByClassName(
+//   'playbackSoundBadge__titleLink sc-truncate'
+// )[0].attributes[0].nodeValue
